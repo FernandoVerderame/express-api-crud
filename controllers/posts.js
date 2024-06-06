5695e546b435e8d39f5cd06da7c787ebac957836
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-06 16:00:46 +0200

    added controller posts

---
+++ controllers/posts.js
@@ -0,0 +1,36 @@
+// Importo PrismaClient
+const { PrismaClient } = require("@prisma/client");
+
+// Inizializzo Prisma
+const prisma = new PrismaClient();
+
+// Importo la funzione per generare lo slug
+const createSlug = require("../utils/slug.js");
+
+// Store dei Posts
+const store = async (req, res) => {
+
+    const { title, content } = req.body;
+
+    // Genero lo slug
+    const slug = createSlug(title);
+
+    const data = {
+        title,
+        slug: slug,
+        image: req.body.image ? req.body.image : '',
+        content,
+        published: req.body.published ? true : false
+    }
+
+    try {
+        const post = await prisma.post.create({ data });
+        res.status(200).send(post);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {
+    store
+}
