7495daebc871003ae256534ea08c81573a3ee859
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-06 14:45:21 +0200

    created app

---
+++ app.js
@@ -0,0 +1,23 @@
+// Importo il path
+const path = require("path")
+
+// Importo express e dotenv
+const express = require('express');
+const dotenv = require("dotenv");
+
+// Inizializzo express
+const app = express();
+
+// Definisco le variabili port e host
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+dotenv.config();
+
+// application/json
+app.use(express.json());
+
+// Avvio il server
+app.listen(port, host, () => {
+    console.log(`Server avviato su http://${host}:${port}`);
+});
